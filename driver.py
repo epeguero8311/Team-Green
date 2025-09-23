1a04d2a378442432f97525b8cf52440d0f156099
Kyleigh <candyrae2006@gmail.com>
2025-09-23 11:15:45 -0500

    Create driver with todos and a few instructions

---
+++ driver.py
@@ -0,0 +1,18 @@
+def main():
+    # First 6 tasks to be completed:
+    # SWE will be completing (2, 4, 6)  and the tester will be completing (1, 3, 5) 
+    # 1. Write code that stores the first N records of the dataset in some custom object.
+    # 2. Write code that calculates the count, mean, std, min, percentiles (25%, 50%, 75%) and max of the age, bmi, children, and charges attributes.
+    # 3. Write code that displays a horizontal text-based histogram of the ages. Remember, you can't use any plotting libraries.
+    # 4. Write code that displays a vertical text-based histogram of the bmi. Remember, you can't use any plotting libraries.
+    # 5. Write code that determines the total number of records for each number of children.
+    # 6. Write code that displays a vertical text-based histogram showing the number of smokers and the number of non-smokers.
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
