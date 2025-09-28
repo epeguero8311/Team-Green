3c343d69a2c0844e92d1ec75aba82899fd068213
Emilio <epeguero@xula.edu>
2025-09-27 22:02:43 -0500

    Add Calculations file

---
+++ .history/Calculations_20250927215400.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Calculations {
+
+    public static void main(String[] args) {
+        readCSV("insurance.csv"); 
+    }
+
+    public static void readCSV(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line); 
+            }
+
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+            e.printStackTrace();
+        }
+    }
+}
