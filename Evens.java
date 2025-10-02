b038b4bc9f838c70e1b8ef46264c9abeb19e52da
Chloe White <cwhite29@xula.edu>
2025-10-02 14:20:18 -0500

    Calling stat function on ages

--- Evens.java
+++ Evens.java
@@ -9,5 +9,12 @@ public class Evens {
         records.add(new InsuranceRecord(60, "female", 35.0, 1, "southwest", false, 6000.0));
 
         Calculations calc = new Calculations(records);
+
+        double mean_age = calc.mean("age");
+        double min_age = calc.min("age");
+        double max_age = calc.max("age");
+
+        double mean_bmi = calc.mean("bmi");
+        double std_bmi = calc.std("bmi");
     }
 }
