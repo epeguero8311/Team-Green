faebb797477da0a12dfb262cc71291b2c3d6ee53
Chloe White <cwhite29@xula.edu>
2025-10-03 13:33:49 -0500

    fixed lines thirty and thirty one

--- Task17.java
+++ Task17.java
@@ -19,8 +19,16 @@ public class Task17 {
             }
         }
 
-        double avg_southern_age = Calculations.getStatistics(southern_ages).get("mean");
-        double avg_northern_age = Calculations.getStatistics(northern_ages).get("mean");
+        ArrayList<Double> southern_ages_double = new ArrayList<>();
+        for (Integer age : southern_ages) {
+            southern_ages_double.add(age.doubleValue());
+        }
+        ArrayList<Double> northern_ages_double = new ArrayList<>();
+        for (Integer age : northern_ages) {
+            northern_ages_double.add(age.doubleValue());
+        }
+        double avg_southern_age = Calculations.getStatistics(southern_ages_double).get("mean");
+        double avg_northern_age = Calculations.getStatistics(northern_ages_double).get("mean");
 
         System.out.printf("Southern Smokers -> Avg Age: %.2f%n", avg_southern_age);
         System.out.printf("Northern Smokers -> Avg Age: %.2f%n", avg_northern_age);
