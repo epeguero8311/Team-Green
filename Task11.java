8e176bad25331774bc1cea7ff6d9674be5bcf78d
Chloe White <cwhite29@xula.edu>
2025-10-03 12:35:53 -0500

    Starting task 11

---
+++ Task11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Task11{
+    private ArrayList<InsuranceRecord> records;
+    
+    public Task11(ArrayList<InsuranceRecord> records) {
+        this.records = records;
+    }
+
+
+}
