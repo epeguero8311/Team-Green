9a9fc8ae60e0b1530ab50f88c84e8e291f8f6f76
Chloe White <cwhite29@xula.edu>
2025-10-03 14:13:55 -0500

    updated task21

--- Task21.java
+++ Task21.java
@@ -19,5 +19,24 @@ public class Task21 {
             System.out.println("File not found");
             return;
         }
+
+        double mean_y = mean(children);
+        double mean_x = mean(charges);
+
+        double numerator = 0.0;
+        double denominator = 0.0;
+
+        for(int i = 0; i < children.size(); i++){
+            numerator += (children.get(i) - mean_y) * (charges.get(i) - mean_x);
+            denominator += Math.pow(children.get(i) - mean_y, 2);
+        }
+
+        double slope = numerator / denominator;
+        double intercept = mean_y - slope * mean_x;
+
+        double r = pearson(children, charges);
+        System.out.println("Pearson correlation coefficient: " + r);
+
+        
     }
 }
